fb2475974b8b6a7124099d651b3841d27ed6cb1d
Toma Adrian <sirhadrian21@gmail.com>
2023-10-09 15:44:31 +0300

    basic

--- autoarch.sh
+++ autoarch.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo 'Archlinux install script'
+echo "Archlinux install script"
+
+echo "Make sure you have an internet connection"
+
+# Start installation
+read -rp "Do you want start the installation? (Y/n)" answer
+
+doInstall=${answer:-Y}
+
+case $doInstall in
+[Yy]) echo "Install started...";;
+[Nn]) exit 0 ;;
+*)
+	echo "Incorrect option"
+	exit 1
+	;;
+esac
+
+# Verify boot mode
+bootmode="$(cat /sys/firmware/efi/fw_platform_size)"
+
+if [[ $bootmode -eq 64 ]];then
+  echo "Booted in UEFI mode"
+elif [[ $bootmode -eq 32 ]];then
+  echo "Booted in legacy mode"
+fi
